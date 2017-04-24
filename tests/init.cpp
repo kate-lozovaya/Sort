3a6695a5e789d33a61d0bb2a65873ab04d8aef06
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-24 18:43:34 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,18 +1,14 @@
-#include <complex.hpp>
+#include <sort.hpp>
 #include <catch.hpp>
 
-SCENARIO("matrix init", "[init]") 
+SCENARIO("sort") 
 {
-	Matrix matrix;
-	REQUIRE(matrix.line() == 0);
-	REQUIRE(matrix.column() == 0);
-}
-
-SCENARIO("matrix with params", "[init]") {
-	Matrix matrix(2,3);
-	REQUIRE(matrix.line() == 2);
-	REQUIRE(matrix.column() == 3);
-	for (int i = 0; i < matrix.line(); ++i)
-		for (int j = 0; j < matrix.column(); ++j)
-			REQUIRE(matrix[i][j] == 0;
+	int a[3][3];
+	a[0][0] = 9; a[0][1] = 8; a[0][2] = 7;
+	a[1][0] = 6; a[1][1] = 5; a[1][2] = 4;
+	a[2][0] = 3; a[2][1] = 2; a[2][2] = 1;
+	insertion_sort(&a[0][0], &a[3][2]);
+	REQUIRE(a[0][0] == 1); REQUIRE(a[0][1] == 2); REQUIRE(a[0][2] == 3);
+	REQUIRE(a[1][0] == 4); REQUIRE(a[1][1] == 5); REQUIRE(a[1][2] == 6);
+	REQUIRE(a[2][0] == 7); REQUIRE(a[1][1] == 8); REQUIRE(a[2][2] == 9);
 }
