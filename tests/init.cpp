abcbdf13f558a2fd635497f606b1ea723e063ed0
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-24 19:10:20 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,11 +4,17 @@
 SCENARIO("sort") 
 {
 	int a[3][3];
-	a[0][0] = 9; a[0][1] = 8; a[0][2] = 7;
-	a[1][0] = 6; a[1][1] = 5; a[1][2] = 3;
-	a[2][0] = 2; a[2][1] = 1; a[2][2] = 0;
-	insertion_sort(&a[0][0], &a[3][2]);
-	REQUIRE(a[0][1] == 0); REQUIRE(a[0][1] == 1); REQUIRE(a[0][2] == 2);
-	REQUIRE(a[1][0] == 3); REQUIRE(a[1][1] == 5); REQUIRE(a[1][2] == 6);
-	REQUIRE(a[2][0] == 7); REQUIRE(a[1][1] == 8); REQUIRE(a[2][2] == 9);
+	a[0][0] = 4; a[0][1] = 3; a[0][2] = 1;
+	a[1][0] = 4; a[1][1] = 6; a[1][2] = 5;
+	a[2][0] = 6; a[2][1] = 7; a[2][2] = 5;
+	for (int i = 0; i < 3; ++i)
+	{
+		int * first = &a[i][0];
+		int * last = &a[i][4];
+		for (int j = 0; j < 3; ++j)
+			insertion_sort(first, last);
+	}
+	REQUIRE(a[0][0] == 1); REQUIRE(a[0][1] == 3); REQUIRE(a[0][2] == 4);
+	REQUIRE(a[1][0] == 4); REQUIRE(a[1][1] == 5); REQUIRE(a[1][2] == 6);
+	REQUIRE(a[2][0] == 5); REQUIRE(a[1][1] == 6); REQUIRE(a[2][2] == 7);
 }
