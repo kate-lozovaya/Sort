0afe4bfc40f2a33f1109f4dd76c7d847488b29b1
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-24 19:06:40 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,10 +5,10 @@ SCENARIO("sort")
 {
 	int a[3][3];
 	a[0][0] = 9; a[0][1] = 8; a[0][2] = 7;
-	a[1][0] = 6; a[1][1] = 5; a[1][2] = 4;
-	a[2][0] = 3; a[2][1] = 2; a[2][2] = 1;
+	a[1][0] = 6; a[1][1] = 5; a[1][2] = 3;
+	a[2][0] = 2; a[2][1] = 1; a[2][2] = 0;
 	insertion_sort(&a[0][0], &a[3][2]);
-	 REQUIRE(a[0][1] == 2); REQUIRE(a[0][2] == 3);
-	REQUIRE(a[1][0] == 4); REQUIRE(a[1][1] == 5); REQUIRE(a[1][2] == 6);
+	REQUIRE(a[0][1] == 0); REQUIRE(a[0][1] == 1); REQUIRE(a[0][2] == 2);
+	REQUIRE(a[1][0] == 3); REQUIRE(a[1][1] == 5); REQUIRE(a[1][2] == 6);
 	REQUIRE(a[2][0] == 7); REQUIRE(a[1][1] == 8); REQUIRE(a[2][2] == 9);
 }
