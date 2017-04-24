d89a0589d2d4f5856b5e134dd27fcfb96f714c74
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-24 18:55:54 +0300

    Update sort.hpp

--- include/sort.hpp
+++ include/sort.hpp
@@ -4,7 +4,7 @@ using namespace std;
 template <typename Iterator>
 void insertion_sort(Iterator first, Iterator last)
 {
-	if (first == last)
+	if (first >= last)
 		return;
 	for (Iterator i = first; i != last; ++i)
 		for (Iterator j = i - 1; (j != first) && (*j < *(j - 1)); --j)
